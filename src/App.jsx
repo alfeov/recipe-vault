321268b4aeb2addabe81b09255b24130d41fe0ae
alfeov <alfeove@gmail.com>
2026-06-16 17:19:51 +0300

    basename, BASE_URL, package changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,40 +14,48 @@ import { categoriesLoader } from '@/loaders/categoriesLoader'
 import { categoryLoader } from '@/loaders/categoryLoader'
 import { mealLoader } from '@/loaders/mealLoader'
 
-const router = createBrowserRouter([
+const BASE_URL = import.meta.env.BASE_URL
+
+const router = createBrowserRouter(
+  [
+    {
+      // based on deploy
+      path: '/',
+      Component: Layout,
+      children: [
+        {
+          // Rout that wrap all routes in ErrorBoundary handler
+          Component: ErrorBoundaryOutlet,
+          ErrorBoundary: ErrorBoundary,
+          children: [
+            {
+              index: true,
+              loader: categoriesLoader,
+              Component: Home,
+            },
+            {
+              path: 'Category/:category',
+              loader: categoryLoader,
+              Component: Category,
+            },
+            {
+              path: 'Meal/:mealId',
+              loader: mealLoader,
+              Component: Meal,
+            },
+            {
+              path: '*',
+              Component: Notfound,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    Component: Layout,
-    children: [
-      {
-        // Rout that wrap all routes in ErrorBoundary handler
-        Component: ErrorBoundaryOutlet,
-        ErrorBoundary: ErrorBoundary,
-        children: [
-          {
-            index: true,
-            loader: categoriesLoader,
-            Component: Home,
-          },
-          {
-            path: 'Category/:category',
-            loader: categoryLoader,
-            Component: Category,
-          },
-          {
-            path: 'Meal/:mealId',
-            loader: mealLoader,
-            Component: Meal,
-          },
-          {
-            path: '*',
-            Component: Notfound,
-          },
-        ],
-      },
-    ],
+    basename: BASE_URL,
   },
-])
+)
 
 function App() {
   return <RouterProvider router={router} />
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+
+  return {
+    base: isProd ? '/recipe-vault/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
+  }
 })
